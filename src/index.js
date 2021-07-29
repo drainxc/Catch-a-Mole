eb9652ba7c159d78c086fc04c9efb5e9618c4a61
ldh7228 <ldh7228@gmail.com>
2021-07-29 12:09:55 +0900

    Add point event

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const main = document.getElementById('main');
+const point = document.getElementById('point');
+
+let num = 0;
 
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
@@ -6,11 +9,20 @@ function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-setInterval (function (){
+setInterval(function () {
     width = getRandomIntInclusive(0, 2);
     height = getRandomIntInclusive(0, 2);
     main.children[width].children[height].style.backgroundColor = "red";
-    setTimeout(function() {
+    let pointIncrease = true;
+    main.children[width].children[height].addEventListener('click', function () {
+        if (pointIncrease)
+        num++;
+        point.innerHTML = `${num}`;
+    })
+    setTimeout(function () {
         main.children[width].children[height].style.backgroundColor = "white";
-      }, 300);
+        pointIncrease = false
+        width = null;
+        height = null;
+    }, 500);
 }, 1000);
