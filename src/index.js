9bf9f0cfdffec486d39f19f43e64c7a11413adc8
ldh7228 <ldh7228@gmail.com>
2021-07-29 13:17:46 +0900

    Fix diglett event

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const main = document.getElementById('main');
 const point = document.getElementById('point');
+const playGame = document.getElementById('playGame');
 
 let num = 0;
 
@@ -9,20 +10,27 @@ function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-setInterval(function () {
-    width = getRandomIntInclusive(0, 2);
-    height = getRandomIntInclusive(0, 2);
-    main.children[width].children[height].style.backgroundColor = "red";
-    let pointIncrease = true;
-    main.children[width].children[height].addEventListener('click', function () {
-        if (pointIncrease)
-        num++;
-        point.innerHTML = `${num}`;
-    })
-    setTimeout(function () {
-        main.children[width].children[height].style.backgroundColor = "white";
-        pointIncrease = false
-        width = null;
-        height = null;
-    }, 500);
-}, 1000);
+function start() {
+    setInterval(function () {
+        width = getRandomIntInclusive(0, 2);
+        height = getRandomIntInclusive(0, 2);
+        main.children[width].children[height].style.backgroundImage = "url('../asset/diglett.jpg')";
+        let pointIncrease = true;
+        main.children[width].children[height].addEventListener('click', function () {
+            if (pointIncrease) {
+                main.children[width].children[height].style.backgroundImage = "url('../asset/pain_diglett.jpg')";
+                num++;
+            }
+            point.innerHTML = `${num}`;
+        })
+        setTimeout(function () {
+            main.children[width].children[height].style.backgroundImage = "url('../asset/white.jpg')";
+            pointIncrease = false
+            width = null;
+            height = null;
+        }, 500);
+    }, 1000);
+}
+
+
+playGame.addEventListener('click', start);
